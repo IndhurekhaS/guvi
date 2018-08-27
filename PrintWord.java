59167104eba9179a8ccbdc644bb9efd5bcd006cf
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2018-08-27 17:16:18 +0530

    Create PrintWord.java

---
+++ PrintWord.java
@@ -0,0 +1,15 @@
+public class PrintWord {
+	public static void main(String[] args) {
+		int n;
+		Scanner scan=new Scanner (System.in);
+		System.out.println("Enter the number:");
+		n=scan.nextInt();
+		for(int i=0;i<n;i++)
+		{
+		 System.out.println("Hello");
+		}
+		
+		scan.close();
+		
+	}
+}
